234a66e6ff8f5ecd9aacc714df89cc93566e4d90
slimeyselim <metinjselim@hotmail.co.uk>
2019-04-01 11:24:29 +0100

    style function

--- src/Todo/TodoItem.js
+++ src/Todo/TodoItem.js
@@ -9,15 +9,24 @@ const propTypes = {
 }
 
 //variables
-const itemStyle = {
+/* 2. const itemStyle = {
     backgroundColor: 'yellow'
 }
+*/
 
 export class TodoItem extends Component {
-  render() {
+    getStyle = () => {
+        return {
+            backgroundColor: 'yellow',
+            textDecoration: this.props.todo.achieved ? 'line-through' : 'none'
+        }
+    } 
+  
+    render() {
     return (
       //<div style = {{ backgroundColor : 'yellow'}}>
-      <div style = {itemStyle}>
+      // 2.<div style = {itemStyle}>
+      <div style={this.getStyle()}>
         <p>{this.props.todo.id} - {this.props.todo.name}</p>
       </div>
     )
